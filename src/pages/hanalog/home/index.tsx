5efbbd1f491dae899c7ea198f24e0261f64e1754
orchard <blankymunns@naver.com>
2025-04-26 15:52:06 +0900

    여행만렙 헤더 생성

--- src/pages/hanalog/home/index.tsx
+++ src/pages/hanalog/home/index.tsx
@@ -1,10 +1,29 @@
-function HanalogHome() {
+import { FaArrowLeft, FaTrophy } from 'react-icons/fa';
+import './Header.css'; // CSS 파일 import
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="leftGroup">
+        <button className="iconButton">
+          <FaArrowLeft size={24} />
+        </button>
+        <h1 className="title">여행만렙</h1>
+      </div>
+      <button className="iconButton">
+        <FaTrophy size={24} />
+      </button>
+    </header>
+  );
+};
+
+const HanalogHome = () => {
   return (
     <div>
-      <h1>Hanalog Home</h1>
-      <p>여기가 여행만렙 메인 페이지입니다.</p>
+      <Header />
+      <h2>여기가 여행만렙 메인 페이지입니다.</h2>
     </div>
   );
-}
+};
 
 export default HanalogHome;
